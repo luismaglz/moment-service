ea1e6143afd82e3fedfd365dd3af21684ebf716b
Luis Gonzalez <luismaglz@live.com>
2018-11-17 18:21:11 -0700

    removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { CalendarDayComponent } from './calendar-day/calendar-day.component';
-import { CalendarMonthComponent } from './calendar-month/calendar-month.component';
 import { GetMonthWeeksPipe } from './get-month-weeks.pipe';
 import { GetTwelveMonthsAheadPipe } from './get-twelve-months-ahead.pipe';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CalendarDayComponent, CalendarMonthComponent, GetMonthWeeksPipe, GetTwelveMonthsAheadPipe ],
+  declarations: [ AppComponent, HelloComponent, GetMonthWeeksPipe, GetTwelveMonthsAheadPipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
